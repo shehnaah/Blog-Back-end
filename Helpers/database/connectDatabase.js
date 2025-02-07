9809ce99a6b8e717f1febcf07445126a25a7ace8
shehnaah <shehna0501@gmail.com>
2025-02-07 20:11:11 +0530

    login

--- Helpers/database/connectDatabase.js
+++ Helpers/database/connectDatabase.js
@@ -1,11 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDatabase =async  () => {
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connection Successful");
+    } catch (error) {
+        console.error("MongoDB Connection Failed:", error);
+        process.exit(1);  
+    }
+};
 
-    await mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser : true})
-
-    console.log("MongoDB Connection Successfully")
-
-}
-
-module.exports = connectDatabase
+module.exports = connectDatabase;
