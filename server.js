04bb76b59c9af404adf13f787cb09853900dc86f
shehnaah <shehna0501@gmail.com>
2025-02-10 18:32:20 +0530

    Your new commit message

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ app.use(customErrorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT} - Mode: ${process.env.NODE_ENV}`);
+    console.log(` Server running on port ${PORT} - Mode: ${process.env.NODE_ENV}`);
 });
 
 // Handle Unhandled Promise Rejections
 process.on("unhandledRejection", (err, promise) => {
-    console.error(`🔥 Error: ${err.message}`);
+    console.error(` Error: ${err.message}`);
     server.close(() => process.exit(1));
 });
